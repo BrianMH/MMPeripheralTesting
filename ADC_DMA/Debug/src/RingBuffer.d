254137202f8cbe6bad1b69be588901c3c15d4ac1
Kenmei <1996brianhnenriquez@gmail.com>
2017-12-20 17:48:27 -0800

    Deleted build directories

--- ADC_DMA/Debug/src/RingBuffer.d
+++
@@ -1,4 +0,0 @@
-src/RingBuffer.o: ../src/RingBuffer.c \
- C:/Users/Brian/eclipse-workspace/ADC_DMA/inc/RingBuffer.h
-
-C:/Users/Brian/eclipse-workspace/ADC_DMA/inc/RingBuffer.h:
--- SerialBuffer/Debug/src/RingBuffer.d
+++
@@ -1,4 +0,0 @@
-src/RingBuffer.o: ../src/RingBuffer.c \
- C:/Users/Brian/eclipse-workspace/SerialBuffer/inc/RingBuffer.h
-
-C:/Users/Brian/eclipse-workspace/SerialBuffer/inc/RingBuffer.h:
