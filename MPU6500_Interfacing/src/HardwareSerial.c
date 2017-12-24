c7cf2dac8198bc1459064966a0adccb5a30a8c2b
Kenmei <1996brianhnenriquez@gmail.com>
2017-12-24 15:57:42 -0800

    Added some debugging things. Works now

---
+++ MPU6500_Interfacing/src/HardwareSerial.c
@@ -0,0 +1,190 @@
+#include <RingBuffer.h>
+#include <HardwareSerial.h>
+#include <stm32f4xx.h>
+#include <stm32f4xx_rcc.h>
+#include <stm32f4xx_usart.h>
+#include <misc.h>
+
+void HUSART_begin(USART_TypeDef* usart, uint32_t baud) {
+   USART_InitTypeDef USART_InitStructure;
+   GPIO_InitTypeDef GPIO_InitStructureRx;
+   GPIO_InitTypeDef GPIO_InitStructureTx;
+
+   // Set up GPIO structs
+   GPIO_InitStructureRx.GPIO_Mode = GPIO_Mode_AF;
+   GPIO_InitStructureRx.GPIO_PuPd = GPIO_PuPd_UP;
+   GPIO_InitStructureRx.GPIO_Speed = GPIO_Speed_50MHz;
+   GPIO_InitStructureRx.GPIO_OType = GPIO_OType_PP;
+   GPIO_InitStructureTx.GPIO_Mode = GPIO_Mode_AF;
+   GPIO_InitStructureTx.GPIO_PuPd = GPIO_PuPd_UP;
+   GPIO_InitStructureTx.GPIO_Speed = GPIO_Speed_50MHz;
+   GPIO_InitStructureTx.GPIO_OType = GPIO_OType_PP;
+
+   // Set up USART struct
+   USART_InitStructure.USART_BaudRate = baud;
+   USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+   USART_InitStructure.USART_StopBits = USART_StopBits_1;
+   USART_InitStructure.USART_Parity = USART_Parity_No;
+   USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+//   // Ensure the the usart peripheral is off before setting it up
+//   USART_Cmd(usart, DISABLE);
+//   USART_DeInit(usart);
+
+   // Varying usart entries have varying implementations
+   if(usart == USART2) {      // USART2 ON PA2/PA3
+      // Initialize corresponding clocks
+      RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+      RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+      //Initialize the pins with structs
+      GPIO_InitStructureRx.GPIO_Pin = GPIO_Pin_3;
+      GPIO_InitStructureTx.GPIO_Pin = GPIO_Pin_2;
+      GPIO_Init(GPIOA, &GPIO_InitStructureRx);
+      GPIO_Init(GPIOA, &GPIO_InitStructureTx);
+      GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+      GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+
+      // Initialize USART
+      USART_Init(USART2, &USART_InitStructure);
+
+      // Activate interrupt system (Set up optional precedence at your leisure)
+      USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+      NVIC_EnableIRQ(USART2_IRQn);
+
+      // Initializes the structs meant for this application
+      RingBuffer_init(&rx2Buf);
+      RingBuffer_init(&tx2Buf);
+   }
+
+   USART_Cmd(usart, ENABLE);
+}
+
+uint8_t HUSART_read(USART_TypeDef* usart) {
+   RingBuffer* rxbuf = _HUSART_RET_BUF(usart, __RX);
+
+   return RingBuffer_dequeue(rxbuf);
+}
+
+RingBuffer* _HUSART_RET_BUF(USART_TypeDef* usart, RTX_Type type) {
+   if(usart == USART2)
+      if(type == __RX)
+         return &rx2Buf;
+      else
+         return &tx2Buf;
+   else
+      return (void*)0;
+}
+
+bool HUSART_writeChar(USART_TypeDef* usart, char twrite) {
+   RingBuffer* txbuf = _HUSART_RET_BUF(usart, __TX);
+
+   // buffer is full. transmission failed
+   if(RingBuffer_isFull(txbuf))
+      return false;
+
+   // attempt transmission
+   RingBuffer_queue(txbuf, (uint8_t)twrite);
+   USART_ITConfig(usart, USART_IT_TXE, ENABLE);
+   return true;
+}
+
+bool HUSART_writeString(USART_TypeDef* usart, char* twrite) {
+   RingBuffer* txbuf = _HUSART_RET_BUF(usart, __TX);
+   char curr = *twrite;
+   uint8_t index = 1;
+
+   while(curr != '\0') {
+      // buffer is full. transmission failed
+      if(RingBuffer_isFull(txbuf))
+         return false;
+
+      // check for escape characters
+      if(curr == '\\') {
+         volatile char futchar = *(twrite+(++index));
+         if(futchar == 'n')
+            RingBuffer_queue(txbuf, (uint8_t)'\n');
+         else if(futchar == 't')
+            RingBuffer_queue(txbuf, (uint8_t)'\t');
+         else if(futchar == '\\')
+            RingBuffer_queue(txbuf, (uint8_t)'\\');
+         else
+            index--; // No possible escape char, just use the char as is
+      }
+
+      // attempt transmission
+      RingBuffer_queue(txbuf, (uint8_t)curr);
+      curr = *(twrite + index++);  // grab next char and increment
+   }
+
+   USART_ITConfig(usart, USART_IT_TXE, ENABLE);
+   return true;
+}
+
+bool HUSART_writeInt(USART_TypeDef* usart, int twrite) {
+   RingBuffer* txbuf = _HUSART_RET_BUF(usart, __TX);
+   bool neg = false;
+   int8_t ind = 0;
+
+   // Do int conversion
+   char buf[11] = {0};
+
+   // check if char is negative
+   if(twrite < 0)
+      neg = true;
+
+   // Perform a simple conversion
+   for(; ind < 10; ind++) {
+      buf[ind] = (char)( 48 + (twrite % 10));
+      twrite /= 10;
+      if(twrite == 0) {
+         if(neg)
+            buf[++ind] = '-';
+         break;   // if twrite is now zero, number conversion is done
+      }
+   }
+
+   // now perform the transfer
+   while(ind >= 0) {
+      // buffer is full. transmission failed
+      if(RingBuffer_isFull(txbuf))
+         return false;
+
+      // attempt transmission
+      RingBuffer_queue(txbuf, (uint8_t)buf[ind]);
+      ind--;
+   }
+
+   USART_ITConfig(usart, USART_IT_TXE, ENABLE);
+   return true;
+}
+
+
+bool HUSART_available(USART_TypeDef* usart) {
+   RingBuffer* rxbuf = _HUSART_RET_BUF(usart, __RX);
+
+   return !RingBuffer_isEmpty(rxbuf);
+}
+
+void HUSART_flush(USART_TypeDef* usart){
+   RingBuffer* rxbuf = _HUSART_RET_BUF(usart, __RX);
+   RingBuffer* txbuf = _HUSART_RET_BUF(usart, __TX);
+
+   // Resets buffers by reinitializing them
+   RingBuffer_init(rxbuf);
+   RingBuffer_init(txbuf);
+}
+
+void USART2_IRQHandler(void) {
+   if(USART_GetITStatus(USART2, USART_IT_RXNE) == SET)
+      // Place new char in rx buffer
+      RingBuffer_queue(&rx2Buf, USART_ReceiveData(USART2));
+   if(USART_GetITStatus(USART2, USART_IT_TXE) == SET) {
+      // Send more characters from buffer
+      if(RingBuffer_isEmpty(&tx2Buf))
+         USART_ITConfig(USART2, USART_IT_TXE, DISABLE);  // empty. done sending
+      else
+         USART_SendData(USART2, RingBuffer_dequeue(&tx2Buf)); // still have more to send
+   }
+}
--- MPU6500_Interfacing/src/main.c
+++ MPU6500_Interfacing/src/main.c
@@ -14,16 +14,17 @@
 #define _CLOCK_PIN GPIO_Pin_13    // PB13
 #define _DATAIN_PIN GPIO_Pin_14   // PB14
 #define _DATAOUT_PIN GPIO_Pin_15  // PB15
-#define _CS_PIN GPIO_Pin_1   	  // PB1
+#define _CS_PIN GPIO_Pin_12   	  // PB12
 #define BUF_SIZE 12
 
-// Registers
-#define USER_CTRL_REG 0x6A
+#define DEBUG_ON 1
 
 // Includes
 #include "stm32f4xx.h"
 #include <stm32f4xx_spi.h>
 #include <stm32f4xx_rcc.h>
+#include <HardwareSerial.h>
+#include "MPU6500.h"
 
 // Structures
 typedef struct IMU_vals{
@@ -33,9 +34,72 @@ typedef struct IMU_vals{
 	int gyro_x;
 	int gyro_y;
 	int gyro_z;
-} IMU_vals;
+} IMU_Vals;
+IMU_Vals MPU6500 = {};	// define default variable to assign values to
 
-// Some helper methods to use
+typedef enum {
+	SPI_CONTINUE,
+	SPI_LAST
+} SPITransferMode;
+
+// (Experimental) Timer methods
+volatile uint32_t multiplier;
+
+void TIM_Delay_Init() {
+	RCC_ClocksTypeDef RCC_Clocks;
+
+	// Use internal clock to scale delays
+	RCC_GetClocksFreq(&RCC_Clocks);
+	multiplier = RCC_Clocks.HCLK_Frequency / 4000000;
+}
+
+void TIM_Delay_Micro(uint32_t msecs) {
+	uint32_t millis = 1000*msecs*multiplier - 10;
+	while(millis--) {}; // delay happens here
+}
+
+// Some helper methods to use for MPU6500
+void MPU6500_WriteToReg(uint8_t address, uint8_t data, SPITransferMode t_mode) {
+	 GPIO_ResetBits(GPIOB, _CS_PIN);
+
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_TXE));	// wait until tx buffer empty
+	SPI_I2S_SendData(SPI2, address);	// send a packet
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_RXNE)); // wait until rx buffer not empty
+	SPI_I2S_ReceiveData(SPI2);
+
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_TXE));
+	SPI_I2S_SendData(SPI2, data);
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_RXNE));
+	SPI_I2S_ReceiveData(SPI2);
+
+	if(t_mode == SPI_LAST)
+		GPIO_SetBits(GPIOB, _CS_PIN);
+}
+
+uint8_t MPU6500_ReadFromReg(uint8_t address, SPITransferMode t_mode) {
+	GPIO_ResetBits(GPIOB, _CS_PIN);
+
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_TXE));
+	SPI_I2S_SendData(SPI2, 0x80|address);
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_RXNE));
+	SPI_I2S_ReceiveData(SPI2); //Clear RXNE bit
+
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_TXE));
+	SPI_I2S_SendData(SPI2, 0x00); //Dummy byte to generate clock
+	while(!SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_RXNE));
+
+	if(t_mode == SPI_LAST)
+		GPIO_SetBits(GPIOB, _CS_PIN);
+
+	return SPI_I2S_ReceiveData(SPI2);
+}
+
+// Updates the global variable MPU6500 that holds the values for the struct
+// Uses the burst read feature to grab the values
+void MPU6500_GetAccGyro_Values() {
+
+}
+
 void MPU6500_Init() {
 	// Init clocks
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOC, ENABLE);
@@ -53,58 +117,65 @@ void MPU6500_Init() {
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, GPIO_AF_SPI2);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource15, GPIO_AF_SPI2);
 
-	// CS pin (enable/disable) pin is a regular pin
+	// and set up SS pin
 	GPIO_InitTypeDefStruct.GPIO_Pin = _CS_PIN;
-	GPIO_InitTypeDefStruct.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitTypeDefStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitTypeDefStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitTypeDefStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOB, &GPIO_InitTypeDefStruct);
+	GPIO_SetBits(GPIOB, _CS_PIN);
 
 	// Initialize SPI capabilities (See MPU6500 data sheet, p.34, for more information)
 	SPI_InitTypeDef SPI_InitTypeDefStruct;
-	SPI_InitTypeDefStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex;	// tx/rx duplex
+	SPI_InitTypeDefStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex;	// tx/rx
 	SPI_InitTypeDefStruct.SPI_Mode = SPI_Mode_Master;
 	SPI_InitTypeDefStruct.SPI_DataSize = SPI_DataSize_8b;	// 1 byte per tx
 	SPI_InitTypeDefStruct.SPI_CPOL = SPI_CPOL_Low;   // data latched on rising edge
 	SPI_InitTypeDefStruct.SPI_CPHA = SPI_CPHA_1Edge;   // data transitioned on falling edge
-	SPI_InitTypeDefStruct.SPI_NSS = SPI_NSS_Soft;	// unused so doesn't matter
-	SPI_InitTypeDefStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256;   // maximum SCLK of 1MHz
+	SPI_InitTypeDefStruct.SPI_NSS = SPI_NSS_Soft | SPI_NSSInternalSoft_Set;
+	SPI_InitTypeDefStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;   // maximum SCLK of 1MHz
 	SPI_InitTypeDefStruct.SPI_FirstBit = SPI_FirstBit_MSB;  // Data is delivered on MSB first
-	SPI_InitTypeDefStruct.SPI_CRCPolynomial = 7;   // unused since no CRC available
+	SPI_InitTypeDefStruct.SPI_CRCPolynomial = 0;   // unused since no CRC available
 	SPI_Init(SPI2, &SPI_InitTypeDefStruct);
 	SPI_Cmd(SPI2, ENABLE);
 
-	// Ensure module does not enter I2C mode
-	MPU6500_WriteToReg(USER_CTRL_REG, 0b00100000);
-}
+	// Places a delay for the loop to set itself up and then allow reading from device
+	TIM_Delay_Micro(1000);
 
-void MPU6500_WriteToReg(uint8_t address, uint8_t data) {
-	SPI_I2S_SendData(SPI2, 0x00|address);   // sets the mode to denote write to add
-	SPI_I2S_SendData(SPI2, data);  			// one byte of data is sent
-}
+	/** The following reset procedure is defined on p. 42 of the MPU6500 register map**/
+	// Reset device
+	MPU6500_WriteToReg(MPU6500_RA_PWR_MGMT_1, 0x80, SPI_LAST);
+	TIM_Delay_Micro(100);
+	// Reset sensors
+	MPU6500_WriteToReg(MPU6500_RA_SIGNAL_PATH_RESET, 0x05, SPI_LAST);
+	TIM_Delay_Micro(100);
+	// Finally set SPI mode permanently and reset other components
+	MPU6500_WriteToReg(MPU6500_RA_USER_CTRL, 0x10|0x08|0x04|0x01, SPI_LAST);
+	TIM_Delay_Micro(1000);
 
-/**
- * This method can possibly generate memory leaks!!!!! Use it wisely...
- */
-uint8_t* MPU6500_ReadFromReg(uint8_t address, uint8_t num_bytes) {
-	if(num_bytes<1)
-		return (void*)0;
+#ifdef DEBUG_ON
+	// Tiny self-reassurance using WHO_AM_I register
+	uint8_t id = MPU6500_ReadFromReg(MPU6500_RA_WHO_AM_I, SPI_LAST);
+	HUSART_writeInt(USART2, id);	// should be 112
+	HUSART_writeString(USART2, "\tend\n");
+#endif
 
-	uint8_t buffer[BUF_SIZE] = malloc(sizeof(uint8_t)*BUF_SIZE);	// create buffer
-	SPI_I2S_SendData(SPI2, 0x80|address);	// sets the mode to denote read from add
+	// Now configure various settings for sensors (see register map)
+	// By default, accelerometer has 1kHz sampling rate w/ 460 Hz bandwidth
+	// By default, gyroscope has 250 Hz bandwidth w/ 4kHz sampling rate (+250 dps)
+	MPU6500_WriteToReg(MPU6500_RA_CONFIG, 0x01, SPI_LAST); // set gyro to 184hz bandwidth
+	MPU6500_WriteToReg(MPU6500_RA_FIFO_EN, MPU6500_FIFO_EN_ACC | MPU6500_FIFO_EN_GYRO, SPI_LAST); // enable accelerometer and gyro FIFO
 
-	for(uint8_t count=0; count<num_bytes; count++) {
-		while(SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_BSY)) {}	// wait for possible tx/rx to end
-		buffer[count] = SPI_I2S_ReceiveData(SPI2);    // burst read for any given bytes
-	}
-
-	return &buffer;   // make sure to free this!
+	// Finally, enable FIFO buffer and disable I2C switching
+	MPU6500_WriteToReg(MPU6500_RA_USER_CTRL, 0x40|0x10, SPI_LAST);
 }
 
 int main(void)
 {
+	TIM_Delay_Init();
+	HUSART_begin(USART2, 9600);
 	MPU6500_Init();
 
-	// While loop
+	// perma loop
 	for(;;) {
 		;
 	}
